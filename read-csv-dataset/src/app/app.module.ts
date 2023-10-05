4fe4bfa43ddffaf0c86325d5d0eedd3cd76e4b7e
houmada <oussamahoumada4@gmail.com>
2023-10-05 15:18:11 +0200

    build

--- read-csv-dataset/src/app/app.module.ts
+++ read-csv-dataset/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './modules/material/material.module';
 import { AppRoutingModule } from './modules/routing/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { D3jsDataReaderComponent } from './components/d3js-data-reader/d3js-data-reader.component';
 import { ChartDialogComponent } from './components/chart-dialog/chart-dialog.component';
+import { D3jsDataReaderComponent } from './components/d3js-data-reader/d3js-data-reader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    ChartDialogComponent,
     D3jsDataReaderComponent,
-    ChartDialogComponent
   ],
   imports: [
     BrowserModule,
